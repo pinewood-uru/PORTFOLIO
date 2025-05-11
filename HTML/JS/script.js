71e83bc595e501a240c61e636be9353ddf62f42d
fran <f.avellanal96@gmail.com>
2025-05-11 18:24:58 -0300

    Domingo

--- HTML/JS/script.js
+++ HTML/JS/script.js
@@ -1,3 +1,5 @@
+// COLLAPSE -------
+
 // Selecciono todos los botones con la clase "collapse"
 const buttons = document.querySelectorAll(".collapse");
 
@@ -11,6 +13,51 @@ buttons.forEach(button => {
     const content = this.nextElementSibling;
 
     // Verifico si el elemento hermano está visible o no
-    content.style.display = content.style.display === "block" ? "none" : "block";
+    content.style.display = content.style.display === "flex" ? "none" : "flex";
   });
 });
+
+// CURSOR-------- https://codepen.io/OuterVale/embed/MYWQQoR?default-tab=result&theme-id=1
+
+const cursor = document.querySelector(".cursor");
+
+// Position cursor div to cursor position
+document.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+});
+
+// Add 'click' class to cursor on mousedown and remove on mouseup
+document.addEventListener("mousedown", (e) => cursor.classList.add("click"));
+document.addEventListener("mouseup", (e) => cursor.classList.remove("click"));
+
+// Add 'pressable' class to cursor when hovering certain elements
+const items = document.querySelectorAll('a, button');
+items.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('pressable');
+  });
+  
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('pressable');
+  });
+});
+
+// LOADER ------- https://css-loaders.com/clones/
+
+const loader = document.getElementById('ctn-loader');
+
+window.addEventListener('load', () => {
+
+  loader.classList.add('hide');
+  setTimeout(() => {
+    loader.style.display = 'none';
+    window.scrollTo({ top: 0 });
+    document.body.style.overflowY = 'auto';
+  }, 2000);
+});
+
+loader.classList.add('show');
+document.body.style.overflowY = 'hidden';
